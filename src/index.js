9af0c86cd3e53f68a6108ddb4110c45e59d8f4e9
KAREN RAMIREZ <karen.bussines@hotmail.com>
2019-11-07 09:28:01 -0600

    se agrega el diseño en CSS y se agrega el Offset en index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,24 @@
+// ESTA FUNCION GUARDA EL VALOR DEL OFFSET 
+function valorOffset(){
+  let objOffset = parseInt(document.getElementById("range").value);
+  return objOffset
+};
+
+// ESTA FUNCION GUARDA EL VALOR DE TEXTO A CIFRAR
 function guardarValorCif(){
-    let saveCifrado = document.getElementById('valorACifrar').value;
-    return saveCifrado;
-    }
+  let saveCifrado = document.getElementById('valorACifrar').value;
+  return saveCifrado;
+};
     
-    
-    function clickCifrar(){
-        console.log(typeof(guardarValorCif()));
-      return clickCifrar;
-    }
+// ESTA FUNCION GUARDA EL VALOR DE TEXTO A DECIFRAR
+function guardarValorDes(){
+  let saveCifrado = document.getElementById('valorADecifrar').value;
+  return saveCifrado;
+};
 
-    function guardarValorDes(){
-        let saveCifrado = document.getElementById('valorADecifrar').value;
-        return saveCifrado;
-        }
-           
-        
-        function clickDecifrar(){
-            console.log(typeof(guardarValorDes()));
-          return clickDecifrar;
-        }
-        
-        let arrayCodigoAcci = []
-        
-        arrayCodigoAcci [65]="A";
-        arrayCodigoAcci [66]="B";
-        arrayCodigoAcci [67]="C";
-        arrayCodigoAcci [68]="D";
-        arrayCodigoAcci [69]="E";
-        arrayCodigoAcci [70]="F";
-        arrayCodigoAcci [71]="G";
-        arrayCodigoAcci [72]="H";
-        arrayCodigoAcci [73]="I";
-        arrayCodigoAcci [74]="I";
-        arrayCodigoAcci [75]="K";
-        arrayCodigoAcci [76]="L";
-        arrayCodigoAcci [77]="M";
-        arrayCodigoAcci [78]="N";
-        arrayCodigoAcci [79]="O";
-        arrayCodigoAcci [80]="P";
-        arrayCodigoAcci [81]="Q";
-        arrayCodigoAcci [82]="R";
-        arrayCodigoAcci [83]="S";
-        arrayCodigoAcci [84]="T";
-        arrayCodigoAcci [85]="U";
-        arrayCodigoAcci [86]="V";
-        arrayCodigoAcci [87]="W";
-        arrayCodigoAcci [88]="X";
-        arrayCodigoAcci [89]="Y";
-        arrayCodigoAcci [90]="Z";
 
- 
+
+
+
+
+
