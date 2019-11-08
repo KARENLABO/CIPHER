36c1c31e2c84cf443f8113cad65d4840ed66a301
KAREN RAMIREZ <karen.bussines@hotmail.com>
2019-11-08 08:28:19 -0600

    se agrega el cipher funcional, se agrega evento para borrar los botones de cifrar y decifrar, se agragan imagenes a pantalla principal

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,33 @@
 window.cipher =  {
+    encode: (offset, string) => {
+        let cifrarTexto="";
+        for(let i=0; i<string.length; i++){
+        let x= string.charCodeAt(i);
+            if (x >64 && x <91){
+            let numAcci= (x-65 + offset) %26 + 65;
+            cifrarTexto +=  String.fromCharCode(numAcci);
+        }else{
+            cifrarTexto +=  String.fromCharCode(x);
+            }
+        }
+        return cifrarTexto;   
+    },
 
-    
+
+    decode:(offset, string) => {
+        let descifrarTexto="";
+        for(let i=0; i<string.length; i++){
+        var x= string.charCodeAt(i);
+            if (x >64 && x <91){
+                let numAcci2= (x+65-offset)%26 + 65;
+                descifrarTexto +=  String.fromCharCode(numAcci2);
+            }else{
+                descifrarTexto +=  String.fromCharCode(x);
+            }
+        }
+        
+        return descifrarTexto;
+    }    
+            
+
 };
-
--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 // ESTA FUNCION GUARDA EL VALOR DEL OFFSET 
 function valorOffset(){
   let objOffset = parseInt(document.getElementById("range").value);
-  return objOffset
-};
+  return objOffset;
+}
 
 // ESTA FUNCION GUARDA EL VALOR DE TEXTO A CIFRAR
 function guardarValorCif(){
   let saveCifrado = document.getElementById('valorACifrar').value;
-  return saveCifrado;
-};
-    
+  return saveCifrado.toUpperCase();
+}
+
+ let mostrarCifra =document.getElementById('valorACifrar');
+ mostrarCifra.addEventListener("keyup",cifra);
+ 
+
 // ESTA FUNCION GUARDA EL VALOR DE TEXTO A DECIFRAR
 function guardarValorDes(){
   let saveCifrado = document.getElementById('valorADecifrar').value;
-  return saveCifrado;
-};
+  return saveCifrado.toUpperCase();
+}
+
+let mostrardescifra =document.getElementById('valorADecifrar');
+mostrardescifra.addEventListener("keyup",decifra);
 
 
+function cifra(){
+  let offSet= parseInt(valorOffset());
+  let string= guardarValorCif();
+  let valueCifra=window.cipher.encode(offSet,string);
+  document.getElementById("valorADecifrar").value = valueCifra;
+}
+
+function decifra(){
+  let offSet= parseInt(valorOffset());
+  let string= guardarValorDes();
+  let valueDecifra= window.cipher.decode(offSet,string);
+  document.getElementById("valorACifrar").value = valueDecifra;
+}
 
 
 
